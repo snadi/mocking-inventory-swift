9bc870b5bffb36586be6b787c2e67095db18b561
Omar <olr7742@nyu.edu>
2024-11-06 15:57:42 +0400

    Fixed duplicate directory creation

--- generate_mocks.sh
+++ generate_mocks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 cd "$(dirname "$0")"
-mkdir Tests/Mocks
+mkdir -p Tests/Mocks
 swift package describe --type json > project.json
 .build/checkouts/mockingbird/mockingbird generate --project project.json \
   --output-dir Tests/Mocks \
